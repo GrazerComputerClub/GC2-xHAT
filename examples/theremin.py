9a212b70aa4806b33d0a9047160ac367f7aca847
mstroh76 <m.stroh76@gmail.com>
2020-05-03 16:51:55 +0200

    Update theremin.py

--- examples/theremin.py
+++ examples/theremin.py
@@ -1,10 +1,8 @@
-pi@raspberrypi:~ $ cat theremin.py 
 from gpiozero import TonalBuzzer, DistanceSensor
 from time import sleep
 
 buzzer = TonalBuzzer(18)
-ds= DistanceSensor(echo=27, trigger=17)
-
+ds = DistanceSensor(echo=27, trigger=17)
 buzzer.source = ds
 
 while True:  
